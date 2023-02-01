5bf971ac6acf16ed8806536abf9ddb48183983d2
Antonio <antonicut@gmail.com>
2023-02-01 20:30:33 +0100

    Creacion saludo.

--- src/versiones/ClasePrincipal.java
+++ src/versiones/ClasePrincipal.java
@@ -3,8 +3,11 @@ package versiones;
 public class ClasePrincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String nombre = "Antonio";
+		System.out.println("Hola " + nombre);
 
+		
 	}
 
 }
